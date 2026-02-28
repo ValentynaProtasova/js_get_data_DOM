b871922a283a734b4fd2ab58c32623fa55a09a86
Valentyna <pr.valya1989@gmail.com>
2026-02-28 20:40:21 +0200

    being formatted correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,13 @@ const numb = texts.map((el) => Number(el.replace(/,/g, '')));
 
 const total = numb.reduce((n1, n2) => n1 + n2, 0);
 
-const average = (total / numb.length).toFixed(2);
+const average = total / numb.length;
 
 document.querySelector('span.total-population').textContent =
   total.toLocaleString('en-US');
 
 document.querySelector('span.average-population').textContent =
-  average.toLocaleString('en-US');
+  average.toLocaleString('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
